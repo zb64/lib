6e775d34edf8f397e42b83e96a745379b42ca847
Veronan <veronan@pm.me>
2020-03-24 01:52:59 +0800

    Add doc strings for exported variables

--- format.go
+++ format.go
@@ -10,25 +10,28 @@ const (
 	lzwLitWidth = 8
 )
 
-var (
-	Deflate = newDataFormat(
-		func(buf io.Writer) io.WriteCloser {
-			w, _ := flate.NewWriter(buf, flate.BestCompression)
-			return w
-		},
-		func(buf io.Reader) (io.ReadCloser, error) {
-			return flate.NewReader(buf), nil
-		})
-	LzwMSB = newDataFormat(
-		func(buf io.Writer) io.WriteCloser {
-			return lzw.NewWriter(buf, lzw.MSB, lzwLitWidth)
-		}, func(buf io.Reader) (io.ReadCloser, error) {
-			return lzw.NewReader(buf, lzw.MSB, lzwLitWidth), nil
-		})
-	LzwLSB = newDataFormat(
-		func(buf io.Writer) io.WriteCloser {
-			return lzw.NewWriter(buf, lzw.LSB, lzwLitWidth)
-		}, func(buf io.Reader) (io.ReadCloser, error) {
-			return lzw.NewReader(buf, lzw.LSB, lzwLitWidth), nil
-		})
-)
+// Deflate is a wrapper of DEFLATE compressed data format, provided by compress/flate.
+var Deflate = newDataFormat(
+	func(buf io.Writer) io.WriteCloser {
+		w, _ := flate.NewWriter(buf, flate.BestCompression)
+		return w
+	},
+	func(buf io.Reader) (io.ReadCloser, error) {
+		return flate.NewReader(buf), nil
+	})
+
+// LzwMSB is a wrapper of Lempel-Ziv-Welch compressed data format using Most Significant Bits, provided by compress/lzw.
+var LzwMSB = newDataFormat(
+	func(buf io.Writer) io.WriteCloser {
+		return lzw.NewWriter(buf, lzw.MSB, lzwLitWidth)
+	}, func(buf io.Reader) (io.ReadCloser, error) {
+		return lzw.NewReader(buf, lzw.MSB, lzwLitWidth), nil
+	})
+
+// LzwMSB is a wrapper of Lempel-Ziv-Welch compressed data format using Least Significant Bits, provided by compress/lzw.
+var LzwLSB = newDataFormat(
+	func(buf io.Writer) io.WriteCloser {
+		return lzw.NewWriter(buf, lzw.LSB, lzwLitWidth)
+	}, func(buf io.Reader) (io.ReadCloser, error) {
+		return lzw.NewReader(buf, lzw.LSB, lzwLitWidth), nil
+	})
